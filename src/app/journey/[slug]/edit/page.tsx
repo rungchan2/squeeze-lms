84a9e4a4859c2fc0167472161bf213710b8425c5
heechan <leeh09077@gmail.com>
2025-03-17 18:35:29 +0900

    fix: server side param problem

--- src/app/journey/[slug]/edit/page.tsx
+++ src/app/journey/[slug]/edit/page.tsx
@@ -1,12 +1,13 @@
 import CreateJourneyPage from "@/app/create-journey/CreateJourneyFrom";
 import { getJourney } from "../../actions";
 
-export default async function page({ params }: { params: { slug: string } }) {
-  const { data, error } = await getJourney(params.slug);
+type Params = Promise<{ slug: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { slug } = await params;
+  const { data, error } = await getJourney(slug);
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  return (
-    <CreateJourneyPage initialData={data || undefined} />
-  )
+  return <CreateJourneyPage initialData={data || undefined} />;
 }
